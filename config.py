d9e5fd60fb18483478c463cbc789a1a9afdfc848
DeVarakkoCorp <mistervarako@gmail.com>
2026-09-15 01:57:15 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,16 @@
+# =========================
+# НАСТРОЙКИ FISHL SHOP BOT
+# =========================
+
+# Токен от @BotFather.
+BOT_TOKEN = "8823534172:AAHF4mpfFRDwJmnAbcGHeQjJpP4VxpFKYyc"
+
+# Username менеджера без @.
+MANAGER_USERNAME = "devarapq"
+
+# ID Telegram-чата, куда бот будет отправлять новые заказы.
+# Например: -1001234567890
+# Если оставить 0, заказы не будут отправляться в чат менеджеров.
+ADMIN_CHAT_ID = 5604717862
+
+SHOP_NAME = "Fishl Shop"
